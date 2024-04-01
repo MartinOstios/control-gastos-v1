d14aed03822695dc6ad38da32446db66af65b751
Martin Ostios <martin.ostios.arias@gmail.com>
2024-04-01 17:53:53 -0500

    Se modificó el Readme y se agregó el router para los reportes

---
+++ routers/reportes.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+# from schemas.product import Product
+from fastapi import FastAPI, Body, Query, Path
+from fastapi.responses import JSONResponse
+from typing import Any, Optional, List
+router = APIRouter()
+
