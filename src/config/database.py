03f19f2306c082be2e3d5eafc6bad591b9733d10
MartinOstios <martin.ostios.arias@gmail.com>
2024-05-13 22:58:14 -0500

    Se modificó el archivo config

--- src/config/database.py
+++ src/config/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
 database_url = f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PASS')}@{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
 
 engine = create_engine(database_url)
